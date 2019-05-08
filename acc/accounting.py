5786d965d8577befc48a2402f5a1c69f283de589
enszhou <40054031+enszhou@users.noreply.github.com>
2019-05-08 20:14:17 +0800

    Update accounting.py

--- acc/accounting.py
+++ acc/accounting.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-accountants_cvs = r'C:\Users\enszh\Documents\WeChat Files\wexin1554657859\FileStorage\File\2019-05\FIN_Audit.csv'
+#change the accountants_csv to your csv fie 
+accountants_cvs = r'C:\Users\enszh\Documents\WeChat Files\wexin1554657859\FileStorage\File\2019-05\FIN_Audit.csv' # your csv file
 url_query = 'http://cmispub.cicpa.org.cn/cicpa2_web/PersonIndexAction.do'
 url_html = 'http://cmispub.cicpa.org.cn/cicpa2_web/public/query0/2/00.shtml'
 host = 'http://cmispub.cicpa.org.cn'
-item_num = 5
+item_num = 5 # the number of items you need
 
 
 def get_names():
